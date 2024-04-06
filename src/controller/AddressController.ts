b5361d30c1422f775a4be641164f7daf87c54a45
yunus emre <yunuzemre30@gmail.com>
2024-04-06 13:53:38 +0200

    edit address-userOne

--- src/controller/AddressController.ts
+++ src/controller/AddressController.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { Address } from "../entity/Address";
+import { User } from "../entity/User";
 
 export class AddressController {
   private addressRepository = AppDataSource.getRepository(Address);
+  private userRepository = AppDataSource.getRepository(User);
 
   async all(request: Request, response: Response, next: NextFunction) {
     return this.addressRepository.find({
@@ -38,15 +40,13 @@ export class AddressController {
 
   async userOne(request: Request, response: Response, next: NextFunction) {
     const userId = parseInt(request.params.userId);
-
-    const user = await this.addressRepository.findOne({
+    const user = await this.userRepository.findOne({
       where: { id: userId },
     });
 
     if (!user) {
       return "böyle bir kullanıcı yok";
     }
-
     const address = await this.addressRepository.find({
       where: { user },
       relations: {
